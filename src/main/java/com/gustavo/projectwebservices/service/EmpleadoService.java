3e4d7f46654092824e26c433fd7148b3f8cec1d1
Gustavo Bearzi <gustavobearzi2012@gmail.com>
2023-02-25 11:49:52 -0300

    Nombre del proyecto: project-webservices-v1.0 Descripcion: Se realizan los metodos con peticiones GET Y POST para la creacion de webservices con una entidad de Empleado como ejemplo.

--- src/main/java/com/gustavo/projectwebservices/service/EmpleadoService.java
+++ src/main/java/com/gustavo/projectwebservices/service/EmpleadoService.java
@@ -4,6 +4,8 @@
 package com.gustavo.projectwebservices.service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.gustavo.projectwebservices.entity.Empleado;
 
@@ -13,7 +15,15 @@ import com.gustavo.projectwebservices.entity.Empleado;
  */
 public class EmpleadoService {
 
-	public Empleado consultarEmpleadoPorID(Long id) {
+	public Empleado consultarEmpleadoPorID(String id) {
+		List<Empleado> empleados = this.consultarEmpleados();
+		
+		for (Empleado empleado : empleados) {
+			if (empleado.getNumeroEmpleado().equals(id)) {
+				return empleado;
+			}
+		}
+		
 		return null;
 	}
 	
@@ -26,5 +36,29 @@ public class EmpleadoService {
 		e.setFechaCreacion(LocalDateTime.now());
 		
 		return e;
+	}
+	
+	public List<Empleado> consultarEmpleados(){
+		List<Empleado> empleados = new ArrayList<>();
+		
+		Empleado e = new Empleado();
+		e.setNumeroEmpleado("12345");
+		e.setNombre("Diego");
+		e.setApellido("Lopez");
+		e.setEdad(31);
+		e.setFechaCreacion(LocalDateTime.now());
+		
+		Empleado e1 = new Empleado();
+		e1.setNumeroEmpleado("123456789");
+		e1.setNombre("Mario");
+		e1.setApellido("Bearzi");
+		e1.setEdad(25);
+		e1.setFechaCreacion(LocalDateTime.now());
+		
+		empleados.add(e);
+		empleados.add(e1);
+		
+		return empleados;
+		
 	}
 }
--- src/main/java/com/gustavo/projectwebservices/ws/EmpleadosWS.java
+++ src/main/java/com/gustavo/projectwebservices/ws/EmpleadosWS.java
@@ -3,11 +3,17 @@
  */
 package com.gustavo.projectwebservices.ws;
 
+import java.util.List;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.gustavo.projectwebservices.entity.Empleado;
 import com.gustavo.projectwebservices.service.EmpleadoService;
@@ -33,5 +39,48 @@ public class EmpleadosWS {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Empleado consultarEmpleado() {
 		return this.es.consultarEmpleadoPorID();
+	}
+	
+	@GET
+	@Path("consultarEmpleados")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public List<Empleado> consultarEmpleados(){
+		return this.es.consultarEmpleados();
+	}
+	
+	@GET
+	@Path("consultarEmpleadoPorId/{numEmp}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response consultarEmpleadoPorId(@PathParam("numEmp") String numero) {
+		Empleado em =  this.es.consultarEmpleadoPorID(numero);
+		
+		if(em == null) {
+			return Response.noContent().build();
+		}
+		
+		GenericEntity<Empleado> empleadoGenerico = new GenericEntity<Empleado>(em, Empleado.class);
+		
+		return Response.ok(empleadoGenerico).build();
+	}
+	
+	@POST
+	@Path("guardarEmpleado")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response guardarEmpleado(Empleado emp) {
+		
+		if(emp == null) {
+			return Response.status(400).entity("No se ha ingresado dato alguno del empleado. Favor de ingresar datos").build();
+		}
+		
+		
+		if(emp.getNombre() == null || emp.getNombre().isEmpty()) {
+			return Response.status(400).entity("El nombre es obligatorio.").build();
+		}
+		
+		GenericEntity<Empleado> empleadoGenerico = new GenericEntity<Empleado>(emp, Empleado.class);
+		return Response.ok(empleadoGenerico).build();
 	}
 }
